2e08df48f8d7a8d44345a9e64e21fa9636bd5eef
sudhirke <sudhir.kesharwani@gmail.com>
2024-06-03 17:24:49 +0530

    Advanced React:  created a dedicated starRating componenent in the movie rating project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+//import "./index.css";
+//import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import StarRating from "./StarRating";
+
+// Consumer component that uses StarRating and outputs the value
+function TestComponent() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating maxRating={10} color="purple" onSetRating={setMovieRating} />
+      <p>This was rated {movieRating}</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={"XXXX"} size={24} />
+    <StarRating maxRating={10} color="red" size={24} defaultRating={3} />
+    <TestComponent />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
